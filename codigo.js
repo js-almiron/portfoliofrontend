67bcd2a3d1bf07cce6ce6ad8bc99f3b28d0c2a57
js-almiron <joanasalmiron@gmail.com>
2022-10-04 15:25:49 -0300

    Agregado de archivo JavaScript

---
+++ codigo.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Hola ¿como te llamas?")
+alert("Bienvenido " +  nombre + " a mi Portfolio Web")
+
+// presentacion = "Soy Joana Almiron , te doy la bienvenida a mi PortfolioWeb"
+// alert(presentacion)
+
