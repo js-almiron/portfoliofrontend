b1dd3a8436bb9d7ce42a52e0f84f86907bcb1f58
js-almiron <joanasalmiron@gmail.com>
2022-10-14 14:15:10 -0300

    sumamos prueba.js para practicas de javascript

--- codigo.js
+++ codigo.js
@@ -3,4 +3,3 @@ alert("Bienvenido " +  nombre + " a mi Portfolio Web")
 
 // presentacion = "Soy Joana Almiron , te doy la bienvenida a mi PortfolioWeb"
 // alert(presentacion)
-
---
+++ pruebas.js
@@ -0,0 +1,24 @@
+/* operadores de asignacion 
+let numero1=10;
+let numero2=5;
+resultado=numero1 + numero2;
+alert(resultado)
+*/
+
+/* operadores de concatenacion */
+saludo = "Hola";
+pregunta = "¿como estas?";
+frase= saludo + pregunta;
+document.write(frase)
+
+nombre="Joana Almiron"; 
+// frase= "soy " + nombre + " y estoy caminando"
+frase1= `soy ${nombre} y estoy corriendo`;
+document.write(frase1)
+
+let numero1=10;
+let numero2=15;
+
+resultado= (numero1<numero2) || (numero1==numero2);
+document.write(resultado)
+
